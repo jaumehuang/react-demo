099485a975e5bb438b69d0a382c203085cdc88e0
黄嘉浩 <1332204564@qq.com>
2020-11-21 17:32:19 +0800

    点击事件

--- src/components/father.tsx
+++ src/components/father.tsx
@@ -7,16 +7,17 @@ class Father extends React.Component<any,any> {
         super(props);
     }
     //点击事件并传值
-    // private testClick =  (n: Number) => {
-    //     this.setState({
-    //         count:n
-    //    })
-    // }
+    private testClick =  (n: Number) => {
+        alert('点中了')
+    }
     render() {
         const num = 123456;
         return(
             <div>
                 <div>我是父组件</div>
+                <div>
+                    点击事件：<button onClick={() => this.testClick(4)}>点我</button>
+                </div>
                 <Child datas={num}></Child>
             </div>
            
